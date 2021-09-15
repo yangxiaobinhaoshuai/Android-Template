13f0dd712607dd992d05cb02dae4f7abbe2b04ef
yangxiaobin <yangxiaobin@kuaishou.com>
2021-09-15 11:52:37 +0800

    Initial commit.

---
+++ app/src/main/java/me/yangxiaobin/android/codelab/MainActivity.kt
@@ -0,0 +1,11 @@
+package me.yangxiaobin.android.codelab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
