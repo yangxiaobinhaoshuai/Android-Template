5bc1e257e7895900278a9148ae556c045147e7c3
yangxiaobin <yangxiaobin@kuaishou.com>
2022-09-09 13:25:55 +0800

    Submit comments.

--- codelab-kt/src/main/java/me/yangxiaobin/kotlin/codelab/ext/DelegateExt.kt
+++ codelab-kt/src/main/java/me/yangxiaobin/kotlin/codelab/ext/DelegateExt.kt
@@ -5,6 +5,11 @@ import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
 
+/**
+ * @see kotlin.properties.Delegates
+ * @see kotlin.properties.Delegates.notNull
+ */
+
 object DelegateAdapter{
 
     public inline fun <T> observable(
