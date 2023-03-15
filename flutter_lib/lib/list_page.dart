51204274ce157ef228c718b359270e46295b625c
yangxiaobin <yangxiaobin@kuaishou.com>
2023-03-15 19:35:14 +0800

    Update list page dart.

--- flutter_lib/lib/list_page.dart
+++ flutter_lib/lib/list_page.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_lib/abs_page.dart';
 
 class ListPage extends AppBarPage {
@@ -6,8 +6,43 @@ class ListPage extends AppBarPage {
 
   @override
   Widget buildBody() {
-    return ListView.builder(itemBuilder: buildItem,itemCount: 20,);
+    return ListView.builder(
+      itemBuilder: buildItem,
+      itemCount: 20,
+    );
   }
 
-  Widget buildItem(BuildContext context, int index) => Text(" index : $index");
+  Widget buildItem(BuildContext context, int index) => Draggable(
+        feedback: Container(
+          color: Colors.pinkAccent,
+        ),
+        child: Container(
+          margin: const EdgeInsetsDirectional.symmetric(
+              vertical: 10, horizontal: 20),
+          decoration: BoxDecoration(
+            color: Colors.blueGrey,
+            borderRadius: BorderRadius.circular(40),
+          ),
+          child: Column(
+            children: [
+              Center(
+                child: Container(
+                  height: 40,
+                  padding: const EdgeInsets.only(top: 10),
+                  child: Text(
+                    " index : $index",
+                    style: const TextStyle(
+                      fontSize: 14,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+
+              // list divider
+              //const Divider(),
+            ],
+          ),
+        ),
+      );
 }
