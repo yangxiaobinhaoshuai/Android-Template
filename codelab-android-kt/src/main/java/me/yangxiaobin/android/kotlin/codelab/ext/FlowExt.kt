d1f1895632efb7eda77243f5558ade5b2b56de5a
yangxiaobin01 <yangxiaobin01@zuoyebang.com>
2025-11-27 14:09:25 +0800

    Add groupby to flow ext.

--- codelab-android-kt/src/main/java/me/yangxiaobin/android/kotlin/codelab/ext/FlowExt.kt
+++ codelab-android-kt/src/main/java/me/yangxiaobin/android/kotlin/codelab/ext/FlowExt.kt
@@ -5,7 +5,10 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.merge
+import kotlinx.coroutines.flow.scan
 import kotlinx.coroutines.launch
+import kotlin.collections.orEmpty
+import kotlin.collections.plus
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 
@@ -19,3 +22,12 @@ public fun <T> Flow<T>.launchIn(
 
 
 public operator fun <T> Flow<T>.plus(other: Flow<T>): Flow<T> = merge(this, other)
+
+
+fun <T, K> Flow<T>.groupBy(
+    keySelector: (T) -> K
+): Flow<Map<K, List<T>>> = scan(emptyMap()) { acc, value ->
+    val key = keySelector(value)
+    val list = acc[key].orEmpty() + value
+    acc + (key to list)
+}
